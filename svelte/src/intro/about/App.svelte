bcc8cfa2a40f759d71c76c880fc20e9e75cbe3c2
martinm07 <martin.github07@gmail.com>
2022-06-30 12:27:22 +0100

    Make new About page

--- svelte/src/intro/about/App.svelte
+++ svelte/src/intro/about/App.svelte
@@ -1,25 +1,48 @@
 <script>
-  import inkling from "./assets/inkling_pink.jpg";
-  let count = 0;
+  import { fly } from "svelte/transition";
+  let open = false;
 </script>
 
 <main class="main">
-  <img src={inkling} alt="" />
-  <button on:click={() => count++}>Clicks: {count}</button><br />
-  If I'm able to view this text in a font that isn't monospace, then I'm happy, but
-  less so because I'm more confident that I'll be able to.
+  <div class="btn-container">
+    <button on:click={() => (open = !open)}
+      >{open ? "Close" : "Open"} About section</button
+    >
+  </div>
+  {#if open}
+    <div transition:fly={{ y: -10, duration: 300 }}>
+      Welcome to the about section of nothing in particular.<br />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+      in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  {/if}
 </main>
 
 <style>
   :root {
     --colour-red: #ff0000;
   }
-
   .main {
     color: var(--colour-red);
-    text-decoration: dotted;
+    font-family: "Courier New", Courier, monospace;
+    font-weight: bold;
   }
-  main img {
-    border: 2px solid #000;
+
+  .btn-container {
+    text-align: center;
+  }
+  button {
+    width: 200px;
+    height: 33px;
+  }
+
+  div {
+    width: 50vw;
+    margin-left: 25vw;
+    margin-top: 20px;
   }
 </style>
